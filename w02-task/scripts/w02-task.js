4924d1aa148cebbca843158f004eee6251a8139a
weblearnbasic <brookescleverly@gmail.com>
2023-09-22 21:34:47 -0600

    done with week 2

--- w02-task/scripts/w02-task.js
+++ w02-task/scripts/w02-task.js
@@ -4,33 +4,32 @@
 
 /* Step 2 - Variables */
 let fullName = "Brooke Cleverly";
-let currentYear = date();
-let profilePicture = "images/zoo.jpg";
+let currentYear = new Date().getFullYear();
+let profilePicture = "images/zoo.png";
 
 /* Step 3 - Element Variables */
 const nameElement = document.getElementById("name");
 const foodElement = document.getElementById("food");
 const yearElement = document.querySelector("#year");
-const imageElement = document.getElementsByName(profilePicture);
+const imageElement = document.getElementsByTagName("img")[0];
 
 /* Step 4 - Adding Content */
 nameElement .innerHTML = `<strong>${fullName}</strong>`;
-yearElement .textContent = `<strong>${currentYear}</{strong>}`;
-imageElement .setAttribute("src", profilePicture); 
-imageElement .setAttribute("alt", `Profile image of ${fullName}`);
-
+yearElement .textContent = currentYear;
+imageElement .setAttribute("src", profilePicture);
+imageElement .setAttribute("alt", `Profile image of ${fullName}`)
 
 
 
 /* Step 5 - Array */
-const food = [tomatoes, ice-cream, waffles, salsa, salad, brownie]
-document.getElementById("food").innerHTML = food.toString();
-food.push("grape");
-foodElement.innerHTML += `<br>${food}`;
-food.pop(0);
-foodElement.innerHTML += `<br>${food}`;
-food.pop();
-foodElement.innerHTML += `<br>${food}`;
+const favFood = ["tomatoes", "icecream", "waffles", "salsa", "salad", "brownie"];
+document.getElementById("food").innerHTML = favFood.toString();
+favFood.push("grape");
+foodElement.innerHTML += `<br>${favFood}`;
+favFood.shift();
+foodElement.innerHTML += `<br>${favFood}`;
+favFood.pop();
+foodElement.innerHTML += `<br>${favFood}`;
 
 
 
