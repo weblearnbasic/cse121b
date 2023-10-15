bfb13eba432cf6c53da13409587a89b14d75bce4
weblearnbasic <brookescleverly@gmail.com>
2023-10-14 22:29:17 -0600

    new

--- scripts/w05-task.js
+++ scripts/w05-task.js
@@ -85,7 +85,7 @@ const sortBy = (temples) => {
             displayTemples(temples.filter(temple => !temple.location.includes("Utah")));
             break;
         case "older":
-            displayTemples(temples.filter(temple => valueof(temple.dedication) < valueof(new Date(1950,1,0))));
+            displayTemples(temples.filter(temple => valueof(new Date(temple.dedication)) < valueof(new Date(1950,1,0))));
             break;
         case "all":
            displayTemples(temples);
